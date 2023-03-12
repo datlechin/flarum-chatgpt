cccfa0227309ed1555e256b113c472692880deca
Ngo Quoc Dat <datlechin@gmail.com>
2023-03-12 12:29:48 +0700

    Build assets

--- js/src/admin/index.tsx
+++ js/src/admin/index.tsx
@@ -8,7 +8,7 @@ app.initializers.add('datlechin/flarum-chatgpt', () => {
       type: 'text',
       label: app.translator.trans('datlechin-chatgpt.admin.settings.api_key_label'),
       help: app.translator.trans('datlechin-chatgpt.admin.settings.api_key_help', {
-        a: <a href="https://platform.openai.com/account/api-keys" target="_blank" rel="noopener"/>
+        a: <a href="https://platform.openai.com/account/api-keys" target="_blank" rel="noopener" />,
       }),
       placeholder: 'sk-...',
     })
@@ -20,7 +20,7 @@ app.initializers.add('datlechin/flarum-chatgpt', () => {
       },
       label: app.translator.trans('datlechin-chatgpt.admin.settings.model_label'),
       help: app.translator.trans('datlechin-chatgpt.admin.settings.model_help', {
-        a: <a href="https://platform.openai.com/docs/models/overview" target="_blank" rel="noopener"/>
+        a: <a href="https://platform.openai.com/docs/models/overview" target="_blank" rel="noopener" />,
       }),
     })
     .registerSetting({
@@ -28,7 +28,7 @@ app.initializers.add('datlechin/flarum-chatgpt', () => {
       type: 'number',
       label: app.translator.trans('datlechin-chatgpt.admin.settings.max_tokens_label'),
       help: app.translator.trans('datlechin-chatgpt.admin.settings.max_tokens_help', {
-        a: <a href="https://help.openai.com/en/articles/4936856" target="_blank" rel="noopener"/>
+        a: <a href="https://help.openai.com/en/articles/4936856" target="_blank" rel="noopener" />,
       }),
       default: 100,
     })
@@ -38,10 +38,13 @@ app.initializers.add('datlechin/flarum-chatgpt', () => {
       label: app.translator.trans('datlechin-chatgpt.admin.settings.enable_on_discussion_started_label'),
       help: app.translator.trans('datlechin-chatgpt.admin.settings.enable_on_discussion_started_help'),
     })
-    .registerPermission({
-      label: app.translator.trans('datlechin-chatgpt.admin.permissions.use_chatgpt_assistant_label'),
-      icon: 'fas fa-comment',
-      permission: 'discussion.useChatGPTAssistant',
-      allowGuest: false,
-    }, 'start')
+    .registerPermission(
+      {
+        label: app.translator.trans('datlechin-chatgpt.admin.permissions.use_chatgpt_assistant_label'),
+        icon: 'fas fa-comment',
+        permission: 'discussion.useChatGPTAssistant',
+        allowGuest: false,
+      },
+      'start'
+    );
 });
